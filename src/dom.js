9ab56aad8215d08adc25835b0ea14565bfe61925
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2024-02-05 00:59:18 +0000

    add gif api integration

--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,33 @@
-import { getWeather } from "./weather";
+import { fetchGiffy, getWeather } from "./weather";
 
 export function setupPage() {
+  const dashboard = document.createElement("div");
+  dashboard.setAttribute("id", "dashboard");
+
   const container = document.createElement("div");
   container.setAttribute("id", "container");
-
   container.appendChild(buildSearch());
+  container.appendChild(dashboard);
+
   document.body.appendChild(container);
+
+  display("london");
 }
 
-async function display(location) {
-  const data = await getWeather(location);
+async function display(search) {
+  // get data
+  const data = await getWeather(search);
+  const currentData = data.current;
+  const forecastData = data.forecast;
+  const locationData = data.location;
+
   console.log(data);
+  // clear dashboard
+  const dashboard = document.querySelector("#dashboard");
+  dashboard.innerHTML = "";
+
+  // build dash board displays
+  dashboard.appendChild(buildCurrent(currentData, locationData));
 }
 
 function buildSearch() {
@@ -38,4 +55,42 @@ function buildSearch() {
   form.appendChild(submit);
 
   return form;
+}
+
+function buildCurrent(weatherData, locationData) {
+  // location, temperature, desc, img, time
+  // location
+  const location = document.createElement("h1");
+  location.textContent = locationData.name;
+  // weather list
+  const temp = getListElement(weatherData.temp_c);
+  temp.textContent += "\u2103"; // celcius symbol
+  const description = getListElement(weatherData.condition.text);
+
+  const list = document.createElement("ul");
+  list.appendChild(temp);
+  list.appendChild(description);
+
+  // icon
+  const img = document.createElement("img");
+  fetchGiffy(weatherData.condition.text).then((res) => {
+    img.setAttribute("src", res.data.images.original.url);
+  });
+
+  const container = document.createElement("div");
+  container.appendChild(location);
+  container.appendChild(list);
+  container.appendChild(img);
+
+  return container;
+}
+
+// HELPER FUNCS
+
+function getListElement(data) {
+  const item = document.createElement("li");
+  item.classList.add("CurrentWeatherData");
+  item.textContent = data;
+
+  return item;
 }
--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,22 @@
-export async function getWeather(location) {
+export async function getWeather(search) {
   const KEY = "4a1c8f9e9d7648db823191321240102";
   const response = await fetch(
     "http://api.weatherapi.com/v1/forecast.json?key=" +
       KEY +
       "&q=" +
-      location +
+      search +
       "&days=3"
   );
   return await response.json();
+}
+
+export async function fetchGiffy(searchString) {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=EaWk2ysr6R2q2zsKrVs26JNYPNooIHzO&s=" +
+      searchString,
+    { mode: "cors" }
+  );
+  const gif = await response.json();
+  console.log(gif);
+  return gif;
 }
